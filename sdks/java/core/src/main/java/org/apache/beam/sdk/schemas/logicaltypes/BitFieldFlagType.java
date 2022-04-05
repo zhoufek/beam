4e779e6646c939702c210fea56f3ad030e83bfe6
Zachary Houfek <zhoufek@google.com>
2022-04-05 13:05:09 -0400

    Apply fixes

--- sdks/java/core/src/main/java/org/apache/beam/sdk/schemas/logicaltypes/BitFieldFlagType.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/schemas/logicaltypes/BitFieldFlagType.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.beam.sdk.schemas.logicaltypes;
 
 import static org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.Preconditions.checkArgument;
@@ -12,15 +29,15 @@ import org.apache.beam.sdk.schemas.Schema.FieldType;
 /**
  * Represents a bit field where each bit is a flag that is either set or unset.
  *
- * <p> This field only supports up to 64 bits and has 64 bits regardless of how many flags
- * are provided. All bits to the left of the last flag will always be zero.
+ * <p>This field only supports up to 64 bits and has 64 bits regardless of how many flags are
+ * provided. All bits to the left of the last flag will always be zero.
  */
 @Experimental(Kind.SCHEMAS)
 public final class BitFieldFlagType extends PassThroughLogicalType<Long> {
   public static final String IDENTIFIER = "BitFieldFlag";
-  private final Map<String, Integer> flagToBit;
+  private final Map<String, Byte> flagToBit;
 
-  private BitFieldFlagType(Map<String, Integer> flagToBit) {
+  private BitFieldFlagType(Map<String, Byte> flagToBit) {
     super(IDENTIFIER, FieldType.STRING, "", FieldType.INT64);
     this.flagToBit = flagToBit;
   }
@@ -28,13 +45,13 @@ public final class BitFieldFlagType extends PassThroughLogicalType<Long> {
   /**
    * Creates a field with a bit assigned to each flag in {@code flags}.
    *
-   * <p> The order of flags should generally not matter, but flags will be in order starting from
-   * the rightmost bit. Therefore, given flags A, B, and C in that order, the order in the bit field
+   * <p>The order of flags should generally not matter, but flags will be in order starting from the
+   * rightmost bit. Therefore, given flags A, B, and C in that order, the order in the bit field
    * will be C, B, A.
    */
   public static BitFieldFlagType create(Iterable<String> flags) {
-    Map<String, Integer> flagToBit = new HashMap<>();
-    int i = 0;
+    Map<String, Byte> flagToBit = new HashMap<>();
+    byte i = 0;
     for (String flag : flags) {
       flagToBit.put(flag, i);
       ++i;
@@ -45,17 +62,16 @@ public final class BitFieldFlagType extends PassThroughLogicalType<Long> {
     return new BitFieldFlagType(flagToBit);
   }
 
-  /**
-   * Convenience method with the same behavior as {@link BitFieldFlagType#create(Iterable)}.
-   */
+  /** Convenience method with the same behavior as {@link BitFieldFlagType#create(Iterable)}. */
   public static BitFieldFlagType create(String... flags) {
     return create(Arrays.asList(flags));
   }
 
   /**
-   * Returns a bit field where the bit associated with {@code flag} is set. All other bits will
-   * be zero.
+   * Returns a bit field where the bit associated with {@code flag} is set. All other bits will be
+   * zero.
    */
+  @SuppressWarnings("nullness") // We control the values and only use raw bytes
   public long getWithFlag(String flag) {
     checkArgument(flagToBit.containsKey(flag));
     return 1L << flagToBit.get(flag);
@@ -74,8 +90,8 @@ public final class BitFieldFlagType extends PassThroughLogicalType<Long> {
   }
 
   /**
-   * Convenience method with the same behavior as
-   * {@link BitFieldFlagType#getWithAllFlags(Iterable)}.
+   * Convenience method with the same behavior as {@link
+   * BitFieldFlagType#getWithAllFlags(Iterable)}.
    */
   public long getWithAllFlags(String... flags) {
     return getWithAllFlags(Arrays.asList(flags));
